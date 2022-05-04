001e542611991a699adf219b0c5f897a1b34aa4a
Harry Veilleux <harryveilleux@gmail.com>
2022-05-04 07:08:42 -0400

    Basic skeleton

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='hvframe',
+      version='0.0.1',
+      description='Classes and functions for aliasing variables',
+      author='Harry Veilleux',
+      author_email='harryveilleux@gmail.com',
+      url='https://github.com/HarryVeilleux/hvframe',
+      packages=['hvframe']
+      )
